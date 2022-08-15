f365f588fdf2c3ed6cc48783fcc66d25699afb5d
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-15 12:59:13 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,24 +1,30 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 2.15.0"
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
 
-resource "docker_image" "nginx" {
+resource "docker_image" "juan" {
   name         = "nginx:latest"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "theironhidex"
+  name  = "josue"
   ports {
-    internal = 80
+    internal = 84
     external = 8000
   }
 }
